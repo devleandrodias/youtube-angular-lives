1fab84c2c16ff7eae21a4b6675aaebb584e0f120
devleandrodias <leandrodbdias@gmail.com>
2020-11-19 18:36:46 -0300

    adicionando grid e reponsaividade componente de card

--- agendalive-web/src/app/app.module.ts
+++ agendalive-web/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
@@ -19,6 +20,7 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     BrowserAnimationsModule,
     MatIconModule,
     MatTabsModule,
+    MatCardModule,
     MatButtonModule,
     MatToolbarModule,
   ],
