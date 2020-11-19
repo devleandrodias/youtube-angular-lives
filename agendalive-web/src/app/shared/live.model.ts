d2c07a84bd1668bb992524a267cabc519d1d12bf
devleandrodias <leandrodbdias@gmail.com>
2020-11-19 18:51:32 -0300

    criando models

---
+++ agendalive-web/src/app/shared/live.model.ts
@@ -0,0 +1,9 @@
+export interface ILive {
+  id: string;
+  liveName: string;
+  channelName: string;
+  liveDate: string;
+  liveTime: string;
+  liveLink: string;
+  registrationDate: string;
+}
---
+++ agendalive-web/src/app/shared/response-pageable.model.ts
@@ -0,0 +1,12 @@
+export interface IReponsePageable {
+  content: any[];
+  first: boolean;
+  last: boolean;
+  number: boolean;
+  numberOfelements: number;
+  pageable: any[];
+  size: number;
+  sorte: number;
+  totalElements: number;
+  totalPages: number;
+}
