7b2695016cd05849428de3eb9a67c0c431b7a7bf
devleandrodias <leandrodbdias@gmail.com>
2020-11-21 13:21:32 -0300

    implementando chamada a API em Java

--- agendalive-web/src/app/views/home/live-list/live-list.component.ts
+++ agendalive-web/src/app/views/home/live-list/live-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/model/live.model';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-list',
   templateUrl: './live-list.component.html',
-  styleUrls: ['./live-list.component.css']
+  styleUrls: ['./live-list.component.css'],
 })
 export class LiveListComponent implements OnInit {
+  constructor(private _liveService: LiveService) {}
 
-  constructor() { }
+  livesPrevious: Live[] = [];
+  livesNext: Live[] = [];
 
   ngOnInit(): void {
+    this.getLives();
   }
 
+  getLives() {
+    this._liveService.getLivesWithFlag('previous').subscribe((data) => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+    this._liveService.getLivesWithFlag('next').subscribe((data) => {
+      this.livesNext = data.content;
+      console.log(this.livesNext);
+    });
+  }
 }
